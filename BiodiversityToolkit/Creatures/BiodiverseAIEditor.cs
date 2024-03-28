3e1ed8fb120b6b3a54a18dda7e3f4e31e0a32b86
LoafOrc <33192068+LoafOrc@users.noreply.github.com>
2024-03-28 10:22:47 +1000

    start editor scripts

---
+++ BiodiversityToolkit/Creatures/BiodiverseAIEditor.cs
@@ -0,0 +1,20 @@
+﻿using Biodiversity.General;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEditor;
+using UnityEngine;
+
+namespace BiodiversityToolkit.Creatures;
+[CustomEditor(typeof(BiodiverseAI))]
+public class BiodiverseAIEditor {
+    [DrawGizmo(GizmoType.NonSelected | GizmoType.Selected)]
+    static void DrawGizmos(BiodiverseAI ai, GizmoType gizmoType) {
+        if(ai.searchCoroutine != null) {
+            Gizmos.color = Color.yellow;
+            foreach(GameObject unsearched in ai.currentSearch.unsearchedNodes) {
+                Gizmos.DrawWireSphere(unsearched.transform.position, 1);
+            }
+        }
+    }
+}
