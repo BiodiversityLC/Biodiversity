df82fdf127477cdfe2ca30260ad4de839d8686bb
Ccode-lang <coop112097@gmail.com>
2024-06-17 14:15:14 -0700

    Add config for levels to spawn on

--- Biodiversity/BiodiversityPlugin.cs
+++ Biodiversity/BiodiversityPlugin.cs
@@ -1,4 +1,5 @@
 ﻿using BepInEx;
+using BepInEx.Configuration;
 using BepInEx.Logging;
 using Biodiversity.Creatures.Enemy;
 using Biodiversity.Creatures.HoneyFeeder;
@@ -22,6 +23,9 @@ public class BiodiversityPlugin : BaseUnityPlugin {
     public static BiodiversityPlugin Instance { get; private set; }
     internal new static ManualLogSource Logger { get; private set; }
 
+    static ConfigEntry<string> levelsOgo;
+    static ConfigEntry<string> levelsVermin;
+
     private void Awake() {
         Logger = BepInEx.Logging.Logger.CreateLogSource(MyPluginInfo.PLUGIN_GUID);
         Instance = this;
@@ -38,10 +42,58 @@ public class BiodiversityPlugin : BaseUnityPlugin {
         // TODO: Swap this to LLL once it gets enemy support.
         Logger.LogInfo("Registering the silly little creatures.");
         //Enemies.RegisterEnemy(BiodiverseAssets.HoneyFeeder, Enemies.SpawnType.Outside);
-        Enemies.RegisterEnemy(BiodiverseAssets.Ogopogo, Enemies.SpawnType.Daytime, new Dictionary<LevelTypes, int> { { LevelTypes.MarchLevel, 100 }, { LevelTypes.AdamanceLevel, 100} }, null, BiodiverseAssets.OgopogoNode, BiodiverseAssets.OgopogoKeyword);
-        Enemies.RegisterEnemy(BiodiverseAssets.Vermin, 100, LevelTypes.All, BiodiverseAssets.VerminNode, BiodiverseAssets.VerminKeyword);
+
+        levelsOgo = Config.Bind("General", "Ogopogo Levels", "MarchLevel:100,AdamanceLevel:100", "The moons that Ogopogo will spawn on");
+        levelsVermin = Config.Bind("General", "Vermin Levels", "All:100", "The moons that Vermin will spawn on");
+
+        (Dictionary<LevelTypes, int> OgoLevelType, Dictionary<string, int> OgoCustomLevelType) = SolveLevels(levelsOgo.Value);
+        (Dictionary<LevelTypes, int> VerminLevelType, Dictionary<string, int> VerminCustomLevelType) = SolveLevels(levelsVermin.Value);
+
+
+        Enemies.RegisterEnemy(BiodiverseAssets.Ogopogo, Enemies.SpawnType.Daytime, OgoLevelType, OgoCustomLevelType, BiodiverseAssets.OgopogoNode, BiodiverseAssets.OgopogoKeyword);
+        Enemies.RegisterEnemy(BiodiverseAssets.Vermin, Enemies.SpawnType.Outside, VerminLevelType, VerminCustomLevelType, BiodiverseAssets.VerminNode, BiodiverseAssets.VerminKeyword);
 
         Logger.LogInfo($"{MyPluginInfo.PLUGIN_GUID}:{MyPluginInfo.PLUGIN_VERSION} has loaded!");
+    }
+
+    //Totally didn't copy this from sirenhead because I didn't want to write it again
+    (Dictionary<LevelTypes, int> spawnRateByLevelType, Dictionary<string, int> spawnRateByCustomLevelType) SolveLevels(string config)
+    {
+        Dictionary<LevelTypes, int> spawnRateByLevelType = new Dictionary<LevelTypes, int>();
+        Dictionary<string, int> spawnRateByCustomLevelType = new Dictionary<string, int>();
+
+        string[] configSplit = config.Split(',');
+
+        foreach (string entry in configSplit)
+        {
+            string[] levelDef = entry.Trim().Split(':');
+
+            if (levelDef.Length != 2)
+            {
+                continue;
+            }
+
+            int spawnrate = 0;
+
+            if (!int.TryParse(levelDef[1], out spawnrate))
+            {
+                continue;
+            }
+
+            if (Enum.TryParse<LevelTypes>(levelDef[0], true, out LevelTypes levelType))
+            {
+                spawnRateByLevelType[levelType] = spawnrate;
+                Logger.LogInfo($"Registered spawn rate for level type {levelType} to {spawnrate}");
+            }
+            else
+            {
+                spawnRateByCustomLevelType[levelDef[0]] = spawnrate;
+                Logger.LogInfo($"Registered spawn rate for custom level type {levelDef[0]} to {spawnrate}");
+            }
+        }
+
+
+        return (spawnRateByLevelType, spawnRateByCustomLevelType);
     }
 
     private void NetcodePatcher() {
