5773bffc2f164ee18584c003273808f0d58756b7
Rolevote <123340226+rolevote@users.noreply.github.com>
2024-08-23 22:39:20 +0100

    Create RubberDuckItem.cs

---
+++ Biodiversity/Items/RubberDuck/RubberDuckItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Unity;
+using UnityEngine;
+
+namespace Biodiversity.Items.RubberDuck
+{
+    public class RubberDuckItem : Item
+    {
+        public Material[] Materials;
+    }
+}
