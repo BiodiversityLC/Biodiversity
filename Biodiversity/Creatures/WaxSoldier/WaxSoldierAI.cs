78a28e69f604c985fe7bfc48ab11beffe2168d59
Rolevote <123340226+rolevote@users.noreply.github.com>
2024-08-23 21:43:52 +0100

    Create WaxSoldierAI.cs

---
+++ Biodiversity/Creatures/WaxSoldier/WaxSoldierAI.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Unity;
+
+namespace Biodiversity.Creatures.WaxSoldier
+{
+    public class WaxSoldierAI : EnemyAI
+    {
+        public override void DoAIInterval()
+        {
+            base.DoAIInterval();
+        }
+    }
+}
