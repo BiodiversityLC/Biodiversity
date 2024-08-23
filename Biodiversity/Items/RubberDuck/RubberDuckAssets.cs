91d41ba3fd44fbd801707b0f1e93bf404b215192
Rolevote <123340226+rolevote@users.noreply.github.com>
2024-08-23 23:27:57 +0100

    item code stuff
    there is no way to load it yet

---
+++ Biodiversity/Items/RubberDuck/RubberDuckAssets.cs
@@ -0,0 +1,16 @@
+﻿using Biodiversity.Util.Assetloading;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using LethalLib.Modules;
+
+
+namespace Biodiversity.Items.RubberDuck
+{
+    internal class RubberDuckAssets(string Bundle) : BiodiverseAssetBundle<RubberDuckAssets>(Bundle)
+    {
+        [LoadFromBundle("RubberDuckAsset")] 
+        public Item DuckAsset;
+    }
+}
--- Biodiversity/Items/RubberDuck/RubberDuckItem.cs
+++ Biodiversity/Items/RubberDuck/RubberDuckItem.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using Unity;
+using Unity.Netcode;
 using UnityEngine;
 
 namespace Biodiversity.Items.RubberDuck
@@ -9,5 +10,11 @@ namespace Biodiversity.Items.RubberDuck
     public class RubberDuckItem  : PhysicsProp
     {
         public Material[] Materials;
+        private NetworkVariable<int> RubberDuckVariable = new NetworkVariable<int>(0,default,default);
+        void Awake()
+        {
+            RubberDuckVariable.Value = UnityEngine.Random.Range(0, Materials.Length);
+            GetComponent<MeshRenderer>().materials[0] = Materials[RubberDuckVariable.Value];
+        }
     }
 }
