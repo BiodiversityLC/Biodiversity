3547e6c1b558597f9677495fc27fe29c584444ef
Rolevote <123340226+rolevote@users.noreply.github.com>
2024-09-02 02:45:07 +0100

    wax

--- Biodiversity/Creatures/WaxSoldier/WaxSoldierAI.cs
+++ Biodiversity/Creatures/WaxSoldier/WaxSoldierAI.cs
@@ -1,6 +1,6 @@
 ﻿using GameNetcodeStuff;
 using System;
-using System.Collections.Generic;
+using System.Collections;
 using System.IO;
 using System.Text;
 using Unity;
@@ -14,6 +14,7 @@ namespace Biodiversity.Creatures.WaxSoldier
     public class WaxSoldierAI : EnemyAI
     {
         public NetworkVariable<int> State = new NetworkVariable<int>(0);
+        public BoxCollider StabHITBOX;
         public Vector3 StartPosition;
         public Quaternion StartRotation;
         private AudioSource Theme;
@@ -24,14 +25,14 @@ namespace Biodiversity.Creatures.WaxSoldier
             Theme = GetComponent<AudioSource>();
             StartPosition = transform.position;
             StartRotation = transform.rotation;
-            NetworkBehaviour.Destroy(gameObject);
+            //NetworkBehaviour.Destroy(gameObject);
         }
         public override void DoAIInterval()
         {
             base.DoAIInterval();
             if (State.Value == 0)
             {
-                TargetClosestPlayer(1.5f, true);
+                TargetClosestPlayer(1.5f, true,90);
                 if (agent.remainingDistance <= 0.5f)
                 {
                     creatureAnimator.SetInteger("state", 0);
@@ -64,9 +65,38 @@ namespace Biodiversity.Creatures.WaxSoldier
                     {
                         if (!Theme.isPlaying)
                             Theme.Play();
+                        if (Vector3.Distance(gameObject.transform.position, targetPlayer.gameObject.transform.position) <= 4)
+                        {
+                            SetDestinationToPosition(transform.position);
+                            transform.LookAt(targetPlayer.transform);
+                            StartCoroutine(Stab());
+                        }
                     }
                 }
             }
+        }
+
+        public IEnumerator Stab()
+        {
+            State.Value = 3;
+            creatureAnimator.SetInteger("state", 3);
+            creatureAnimator.speed = .5f;
+            yield return new WaitForSeconds(0.15f);
+            StabHITBOX.gameObject.SetActive(true);
+            foreach (Collider Obj in Physics.OverlapBox(StabHITBOX.center, StabHITBOX.size, Quaternion.identity))
+            {
+                var player = Obj.gameObject.GetComponent<PlayerControllerB>();
+                if (player)
+                {
+                    player.DamagePlayer(30,false,true,CauseOfDeath.Stabbing,0,false, transform.forward * 3);
+                }
+            }
+            StabHITBOX.gameObject.SetActive(false);
+            yield return new WaitForSeconds(0.3f);
+            creatureAnimator.speed = 1f;
+            creatureAnimator.SetInteger("state", 0);
+            yield return new WaitForSeconds(0.1f);
+            State.Value = 1;
         }
         public override void HitEnemy(int force = 1, PlayerControllerB playerWhoHit = null, bool playHitSFX = false, int hitID = -1)
         {
