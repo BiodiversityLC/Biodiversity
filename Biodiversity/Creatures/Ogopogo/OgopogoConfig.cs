544446b2c6e903d9ec4ec1afff08851664ad5f09
montyTLN <ivanmonteros53@gmail.com>
2024-09-12 18:48:38 -0300

    Update OgopogoConfig.cs
    added phaedra to defaults

--- Biodiversity/Creatures/Ogopogo/OgopogoConfig.cs
+++ Biodiversity/Creatures/Ogopogo/OgopogoConfig.cs
@@ -39,7 +39,7 @@ public class OgopogoConfig(ConfigFile configFile) : BiodiverseConfigLoader<Ogopo
 	public bool OgopogoEnabled { get; private set; } = true;
 
 	[field: Tooltip("Spawn weight of the Ogopogo on all moons. WARNING: NO OTHER MOONS OTHER THAN THE ONES PRESENT IN THE DEFAULTS WILL WORK FOR OGOPOGO, HE'S CURRENTLY VERY FINNICKY TO WORK WITH SO HE'S NOT COMPATIBLE WITH MOST MOONS.")]
-	public string OgopogoRarity { get; private set; } = "Vow:10,March:55,Adamance:35,Submersion:15,Seichi:40,Gorgonzola:5,Polarus:0,Aquatis:0,Cesspool:0,Gloom:0";
+	public string OgopogoRarity { get; private set; } = "Vow:10,March:55,Adamance:35,Submersion:15,Seichi:40,Gorgonzola:5,Phaedra:22,Polarus:0,Aquatis:0,Cesspool:0,Gloom:0";
 
 	[field: Tooltip("Moons where Ogopogo's wander is disabled.")]
 	public string OgopogoWanderDisable { get; private set; } = "VowLevel";
