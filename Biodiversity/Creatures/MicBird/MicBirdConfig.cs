eb87bad51eaed668473a05b6c3aa44022367d630
Ccode-lang <coop112097@gmail.com>
2024-10-16 15:57:35 -0700

    add config and update spawn weights

---
+++ Biodiversity/Creatures/MicBird/MicBirdConfig.cs
@@ -0,0 +1,17 @@
+﻿using BepInEx.Configuration;
+using Biodiversity.Util.Config;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace Biodiversity.Creatures.MicBird
+{
+    public class MicBirdConfig(ConfigFile configFile) : BiodiverseConfigLoader<MicBirdConfig>(configFile) {
+        [field: Tooltip("Spawn weight of the Boom bird on all moons.")]
+        public string BoomBirdRarity { get; private set; } = "Experimentation:15,Rend:32,Titan:15,Artifice:36,Atlantica:27,Etern:18,Fission:24,Gratar:10,Polarus:8";
+
+        [field: Tooltip("Whether the Boom bird will spawn in games.")]
+        public bool EnableBoomBird { get; private set; } = false;
+    }
+}
--- Biodiversity/Creatures/MicBird/MicBirdHandler.cs
+++ Biodiversity/Creatures/MicBird/MicBirdHandler.cs
@@ -7,13 +7,15 @@ namespace Biodiversity.Creatures.MicBird
     internal class MicBirdHandler : BiodiverseAIHandler<MicBirdHandler>
     {
         internal MicBirdAssets Assets { get; private set; }
+        internal MicBirdConfig Config { get; private set; }
         public MicBirdHandler()
         {
             Assets = new MicBirdAssets("biodiversity_micbird");
+            Config = new MicBirdConfig(BiodiversityPlugin.Instance.CreateConfig("boombird"));
 
             RegisterEnemyWithConfig(
-                true,
-                "All:20",
+                Config.EnableBoomBird,
+                Config.BoomBirdRarity,
                 Assets.MicBirdEnemyType,
                 Assets.MicBirdTerminalNode,
                 Assets.MicBirdTerminalKeyword);
