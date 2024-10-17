3149d59b0d2b235d98d1530804337604a766ed4d
montyTLN <ivanmonteros53@gmail.com>
2024-10-16 21:16:15 -0300

    Update MicBirdConfig.cs
    added harvest moon weights

--- Biodiversity/Creatures/MicBird/MicBirdConfig.cs
+++ Biodiversity/Creatures/MicBird/MicBirdConfig.cs
@@ -9,7 +9,7 @@ namespace Biodiversity.Creatures.MicBird
 {
     public class MicBirdConfig(ConfigFile configFile) : BiodiverseConfigLoader<MicBirdConfig>(configFile) {
         [field: Tooltip("Spawn weight of the Boom bird on all moons.")]
-        public string BoomBirdRarity { get; private set; } = "Experimentation:15,Rend:32,Titan:15,Artifice:36,Atlantica:27,Etern:18,Fission-C:24,Gratar:10,Polarus:8,Seichi:8,Olympus:4,Arelion:5";
+        public string BoomBirdRarity { get; private set; } = "Experimentation:15,Rend:32,Titan:15,Artifice:36,Atlantica:27,Etern:18,Fission-C:24,Gratar:10,Polarus:8,Seichi:8,Olympus:4,Arelion:5,Fray:1,Sierra:21";
 
         [field: Tooltip("Whether the Boom bird will spawn in games.")]
         public bool EnableBoomBird { get; private set; } = false;
