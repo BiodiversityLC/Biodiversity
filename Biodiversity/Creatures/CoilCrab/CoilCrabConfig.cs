aec34996953e4274ae357e543fb83f1d3a862a3a
montyTLN <ivanmonteros53@gmail.com>
2025-02-15 15:43:24 -0300

    Update CoilCrabConfig.cs
    updated weights

--- Biodiversity/Creatures/CoilCrab/CoilCrabConfig.cs
+++ Biodiversity/Creatures/CoilCrab/CoilCrabConfig.cs
@@ -13,10 +13,10 @@ namespace Biodiversity.Creatures.CoilCrab
         public bool EnableCoilCrab { get; private set; } = true;
 
         [field: Tooltip("Spawn weight of the Coil Crab on all moons when it is stormy.")]
-        public string CoilCrabRarityStormy { get; private set; } = "Offense:45,Rend:22,Titan:25,Artifice:35,Asteroid-13:15,Junic:29,Atlantica:15,Gloom:5,Gratar:53,Acidir:12,Fission:28,Oldred:37,Fray:1,Prominence:20,Seichi:8,Olympus:10";
+        public string CoilCrabRarityStormy { get; private set; } = "Offense:75,Rend:50,Titan:25,Artifice:35,Asteroid-13:15,Junic:29,Atlantica:15,Gloom:5,Gratar:53,Acidir:12,Fission:28,Oldred:37,Prominence:20,Seichi:8,Torus:72,Tundaria:39,Starship-13:22";
 
         [field: Tooltip("Spawn weight of the Coil Crab on all moons when it is not stormy.")]
-        public string CoilCrabRarity { get; private set; } = "Offense:25,Rend:8,Titan:10,Artifice:12,Asteroid-13:9,Junic:18,Atlantica:2,Gloom:4,Gratar:20,Acidir:10,Fission:20,Oldred:20,Fray:1,Prominence:10,Seichi:4,Olympus:7";
+        public string CoilCrabRarity { get; private set; } = "Offense:35,Rend:8,Titan:0,Artifice:12,Asteroid-13:9,Junic:18,Atlantica:2,Gloom:4,Gratar:20,Acidir:10,Fission:20,Oldred:20,Prominence:10,Seichi:4,Torus:38,Tundaria:19,Starship-13:22";
 
         [field: Tooltip("Explosion damage range")]
         public float DamageRange { get; private set; } = 6;
