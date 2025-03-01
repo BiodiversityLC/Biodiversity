75038ea2b7b78bdc1fa73d292f66f4f3e4fa51aa
montyTLN <ivanmonteros53@gmail.com>
2025-03-01 16:19:44 -0300

    Update OgopogoConfig.cs
    removed extra comma

--- Biodiversity/Creatures/Ogopogo/OgopogoConfig.cs
+++ Biodiversity/Creatures/Ogopogo/OgopogoConfig.cs
@@ -39,7 +39,7 @@ public class OgopogoConfig(ConfigFile configFile) : BiodiverseConfigLoader<Ogopo
 	public bool OgopogoEnabled { get; private set; } = true;
 
 	[field: Tooltip("Spawn weight of the Ogopogo on all moons. WARNING: NO OTHER MOONS OTHER THAN THE ONES PRESENT IN THE DEFAULTS WILL WORK FOR OGOPOGO, HE'S CURRENTLY VERY FINNICKY TO WORK WITH SO HE'S NOT COMPATIBLE WITH MOST MOONS.")]
-	public string OgopogoRarity { get; private set; } = "Vow:10,March:65,Adamance:35,Submersion:12,Seichi:40,Gorgonzola:5,Phaedra:22,,Aquatis:0,Cesspool:0,Gloom:0,Bozoros:15,Monarch:5,Oldred:69,Atlantica:52,Polarus:33,Acidir:16,Alcatras:57,Cubatres:8,Filitrios:21,Brutality:25,Phuket:5,Valiance:12,Timbrance:28,Natit:10,Sorrow:10";
+	public string OgopogoRarity { get; private set; } = "Vow:10,March:65,Adamance:35,Submersion:12,Seichi:40,Gorgonzola:5,Phaedra:22,Aquatis:0,Cesspool:0,Gloom:0,Bozoros:15,Monarch:5,Oldred:69,Atlantica:52,Polarus:33,Acidir:16,Alcatras:57,Cubatres:8,Filitrios:21,Brutality:25,Phuket:5,Valiance:12,Timbrance:28,Natit:10,Sorrow:10";
 
 	[field: Tooltip("Moons where Ogopogo's wander is disabled.")]
 	public string OgopogoWanderDisable { get; private set; } = "VowLevel,Submersion,Gorgonzola,Natit,Bozoros,Phaedra";
