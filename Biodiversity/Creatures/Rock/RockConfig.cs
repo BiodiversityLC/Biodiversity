febd5be2f729cd154939bcb93c120f07f8d514c1
montyTLN <ivanmonteros53@gmail.com>
2026-04-12 11:59:57 -0300

    added age to rock weights
    this is a demonstration for Beef and maybe jack u

--- Biodiversity/Creatures/Rock/RockConfig.cs
+++ Biodiversity/Creatures/Rock/RockConfig.cs
@@ -16,7 +16,7 @@ namespace Biodiversity.Creatures.Rock
         public int RockMaxCount { get; private set; } = 3;
 
         [field: Tooltip("Spawn weight of the Rock on all moons.")]
-        public string RockRarity { get; private set; } = "Rend:55,Titan:25";
+        public string RockRarity { get; private set; } = "Rend:55,Titan:25,Age:1";
 
         [field: Tooltip("What material to use for each level. (0 is brown, 1 is grey)")]
         public string RockMatsConfig { get; private set; } = "ExperimentationLevel:0,AssuranceLevel:0,OffenseLevel:0,VowLevel:1,MarchLevel:1,AdamanceLevel:1,RendLevel:1,DineLevel:1,TitanLevel:1,ArtificeLevel:1";
