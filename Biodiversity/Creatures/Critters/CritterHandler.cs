d9dc59752bb6c29768798ed67142735cb8d9c895
ZigzagAwaka <joledoux18@gmail.com>
2026-06-08 16:25:07 +0200

    Added power level configs for Prototax and Leaf boy

--- Biodiversity/Creatures/Critters/CritterHandler.cs
+++ Biodiversity/Creatures/Critters/CritterHandler.cs
@@ -4,28 +4,34 @@ using JetBrains.Annotations;
 namespace Biodiversity.Creatures.Critters;
 
 [UsedImplicitly]
-internal class CritterHandler : BiodiverseAIHandler<CritterHandler> {
-	internal CritterAssets Assets { get; }
-	internal CritterConfig Config { get; }
+internal class CritterHandler : BiodiverseAIHandler<CritterHandler>
+{
+    internal CritterAssets Assets { get; }
+    internal CritterConfig Config { get; }
 
-	public CritterHandler() {
-		Assets = new CritterAssets("critters");
-		Config = new CritterConfig(BiodiversityPlugin.Instance.CreateConfig("critters"));
-		
-		LethalLibUtils.TranslateTerminalNode(Assets.PrototaxTerminalNode);
-		LethalLibUtils.RegisterEnemyWithConfig(
-			Config.FungiEnabled,
-			Config.FungiRarity,
-			Assets.PrototaxEnemyType,
-			Assets.PrototaxTerminalNode,
-			Assets.PrototaxTerminalKeyword);
-		
-		LethalLibUtils.TranslateTerminalNode(Assets.LeafyBoiTerminalNode);
-		LethalLibUtils.RegisterEnemyWithConfig(
-			Config.LeafBoyEnabled,
-			Config.LeafBoyRarity,
-			Assets.LeafyBoiEnemyType,
-			Assets.LeafyBoiTerminalNode,
-			Assets.LeafyBoiTerminalKeyword);
-	}
+    public CritterHandler()
+    {
+        Assets = new CritterAssets("critters");
+        Config = new CritterConfig(BiodiversityPlugin.Instance.CreateConfig("critters"));
+
+        Assets.PrototaxEnemyType.PowerLevel = Config.FungiPowerLevel;
+
+        LethalLibUtils.TranslateTerminalNode(Assets.PrototaxTerminalNode);
+        LethalLibUtils.RegisterEnemyWithConfig(
+            Config.FungiEnabled,
+            Config.FungiRarity,
+            Assets.PrototaxEnemyType,
+            Assets.PrototaxTerminalNode,
+            Assets.PrototaxTerminalKeyword);
+
+        Assets.LeafyBoiEnemyType.PowerLevel = Config.LeafBoyPowerLevel;
+
+        LethalLibUtils.TranslateTerminalNode(Assets.LeafyBoiTerminalNode);
+        LethalLibUtils.RegisterEnemyWithConfig(
+            Config.LeafBoyEnabled,
+            Config.LeafBoyRarity,
+            Assets.LeafyBoiEnemyType,
+            Assets.LeafyBoiTerminalNode,
+            Assets.LeafyBoiTerminalKeyword);
+    }
 }
